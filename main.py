79355190d7d0c9a8801dbc87ec7834087dc0c787
Marcin Kurek <md.kurek@gmail.com>
2020-05-28 14:11:11 +0200

    added exceptions handling

--- main.py
+++ main.py
@@ -13,22 +13,13 @@ from wtforms import StringField
 from wtforms import SelectField
 from wtforms.validators import InputRequired
 
-# Fetch device list using Observium API
+global device_list
 
-r = requests.get(OBSERVIUM_API_BASE_URL + OBSERVIUM_API_DEVICES_IOS, auth=(OBSERVIUM_USERNAME, OBSERVIUM_PASS))
-device_list = []
-devices = r.json()['devices']
-for k,v in devices.items():
-	t = (v['hostname'], v['hostname'])
-	device_list.append(t)
-
-
-class initForm(FlaskForm):
-    
-	device = SelectField('Device', choices=device_list)
-	command = SelectField('Command', choices=[('show interfaces status', 'show interfaces status'), ('show mac address-table vlan', 'show mac address-table vlan')])
-	params = StringField('Params', validators=[InputRequired()])
-	
+class createForm(FlaskForm):
+  
+  device = SelectField('Device')
+  command = SelectField('Command', choices=[('show interfaces status', 'show interfaces status'), ('show mac address-table vlan', 'show mac address-table vlan'), ('show mac address-table interface', 'show mac address-table interface'), ('show vlan id', 'show vlan id')])
+  params = StringField('Params', validators=[InputRequired()])
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '443436456542'
@@ -37,22 +28,50 @@ Bootstrap(app)
 @app.route("/", methods=["GET", "POST"])
 
 def index():
-    form = initForm()
-    if form.validate_on_submit():
-        
-	# Get all params 
-        switch = request.form.get("device")
-        command = request.form.get("command")
-        params = request.form.get("params")
-        conn = ConnectHandler(device_type='cisco_ios', host=switch, username=NET_USERNAME, password=NET_PASSWORD)
-        output = conn.send_command(command)
-        
-	# Initiate checks using data passed to web form
- 
-        return render_template("home.html", form=form, result=output)
-    else:
-        return render_template("home.html", form=form)
 
+# Fetch device list using Observium API
+
+  try:
+    r = requests.get(OBSERVIUM_API_BASE_URL + OBSERVIUM_API_DEVICES_IOS, auth=(OBSERVIUM_USERNAME, OBSERVIUM_PASS))
+  except requests.exceptions.ConnectionError:
+    error = "Unable to connect to Observium API, check your network"
+    return render_template("error.html", error)
+  except requests.exceptions.Timeouts:
+    error = "Unable to fetch device list from Observium API, request timed out"
+    return render_template("error.html", error)
+  except:
+    return render_template("error.html", error="Caught generic exception while connecting to Observium API")
+
+  status = r.status_code
+  if str(status).startswith('5'):
+    return render_template("error.html", error="Server error")
+
+  device_list = []
+  devices = r.json()['devices']
+  for k,v in devices.items():
+    t = (v['hostname'], v['hostname'])
+    device_list.append(t)
+
+
+  form = createForm()   
+  form.device.choices = device_list
+
+  if form.validate_on_submit():
+        
+  	# Get all params 
+    switch = request.form.get("device")
+    command = request.form.get("command")
+    params = request.form.get("params")
+    try:
+      conn = ConnectHandler(device_type='cisco_ios', host=switch, username=NET_USERNAME, password=NET_PASSWORD)
+      output = conn.send_command(command)
+    except:
+      output = "Oops.. something went wrong when connecting to the device"
+
+    
+    return render_template("home.html", form=form, result=output)
+  else:
+    return render_template("home.html", form=form)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
